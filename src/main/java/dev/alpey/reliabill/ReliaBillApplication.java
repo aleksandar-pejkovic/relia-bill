90fff48881f983643c59397b35d32c5dd5d6e976
Aleksandar Pejkovic <aleksandar.pejkovic89@gmail.com>
2023-03-06 11:47:22 +0100

    add checkstyle and liquibase

--- src/main/java/dev/alpey/reliabill/ReliaBillApplication.java
+++ src/main/java/dev/alpey/reliabill/ReliaBillApplication.java
@@ -6,11 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ReliaBillApplication {
 
-	protected ReliaBillApplication() {
-	}
+    protected ReliaBillApplication() {
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(ReliaBillApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ReliaBillApplication.class, args);
+    }
 
 }

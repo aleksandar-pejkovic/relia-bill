abc8ae65e597561809e449c1bb0684c30743baab
Aleksandar Pejkovic <aleksandar.pejkovic89@gmail.com>
2023-03-06 11:11:48 +0100

    first commit

---
+++ src/main/java/dev/alpey/reliabill/ReliaBillApplication.java
@@ -0,0 +1,16 @@
+package dev.alpey.reliabill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReliaBillApplication {
+
+	protected ReliaBillApplication() {
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReliaBillApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/alpey/reliabill/ReliaBillApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alpey.reliabill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReliaBillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

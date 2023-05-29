ff1b2f2073b9f47c846e10e0541ac7d9ea7ef428
aleksandar-pejkovic <aleksandar.pejkovic89@gmail.com>
2023-05-29 13:02:53 +0200

    allow all cors

--- src/main/java/dev/alpey/reliabill/controller/jwt/AuthController.java
+++ src/main/java/dev/alpey/reliabill/controller/jwt/AuthController.java
@@ -2,6 +2,7 @@ package dev.alpey.reliabill.controller.jwt;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ public class AuthController {
     @Autowired
     private TokenService tokenService;
 
+    @CrossOrigin(origins = {"https://reliabill.netlify.app", "http://localhost:5173"})
     @PostMapping("/login")
     public String token(Authentication authentication) {
         return tokenService.generateToken(authentication);

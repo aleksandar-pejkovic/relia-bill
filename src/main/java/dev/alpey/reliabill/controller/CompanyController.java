c8fb447f09d44736020d539a2c417cc66d882f65
aleksandar-pejkovic <aleksandar.pejkovic89@gmail.com>
2023-06-12 18:11:34 +0200

    set global cors config

--- src/main/java/dev/alpey/reliabill/controller/CompanyController.java
+++ src/main/java/dev/alpey/reliabill/controller/CompanyController.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,7 +20,6 @@ import dev.alpey.reliabill.model.dto.CompanyDto;
 import dev.alpey.reliabill.service.CompanyService;
 import jakarta.validation.Valid;
 
-@CrossOrigin(origins = {"https://reliabill.netlify.app", "http://localhost:5173"})
 @RestController
 @RequestMapping("/api/companies")
 public class CompanyController {

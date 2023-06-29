2f85ae619410fa392ce622ebbcf4362bebc70bc9
aleksandar-pejkovic <aleksandar.pejkovic89@gmail.com>
2023-06-29 22:51:07 +0200

    get cors origins value from properties

--- src/main/java/dev/alpey/reliabill/configuration/cors/CorsConfig.java
+++ src/main/java/dev/alpey/reliabill/configuration/cors/CorsConfig.java
@@ -1,5 +1,6 @@
 package dev.alpey.reliabill.configuration.cors;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
+    @Value("${client.url}")
+    private String clientUrl;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "https://reliabill.netlify.app")
+                .allowedOrigins(clientUrl)
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
